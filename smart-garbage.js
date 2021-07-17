222fdfce758e79d0f0eb2257febc80d60a3712c9
Diego Castro <diegojcr4@gmail.com>
2021-07-17 11:56:07 -0400

    Added object manipulating function

---
+++ smart-garbage.js
@@ -0,0 +1,8 @@
+const smartGarbage = function (trash, bins) {
+  //let newBins = bins
+  bins[trash] += 1;
+  return bins;
+}
+
+// Test
+console.log(smartGarbage('recycling', { waste: 4, recycling: 2, compost: 5 }).recycling + '\nRecycling should be at 3.');
